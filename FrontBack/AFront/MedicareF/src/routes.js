e61042a51c3720d2d2b75b87297c047dcef323de
leonrama10 <151144347+leonrama10@users.noreply.github.com>
2025-04-28 12:05:37 +0200

    Ndryshim te fixedPlugin, dhe fshirja e disa filave.

--- FrontBack/AFront/MedicareF/src/routes.js
+++ FrontBack/AFront/MedicareF/src/routes.js
@@ -1,5 +1,4 @@
 /*!
-
 =========================================================
 * Light Bootstrap Dashboard React - v2.0.1
 =========================================================
@@ -11,16 +10,13 @@
 * Coded by Creative Tim
 
 =========================================================
-
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 */
+
 import Dashboard from "views/Dashboard.js";
 import UserProfile from "views/UserProfile.js";
 import TableList from "views/TableList.js";
 import Typography from "views/Typography.js";
-import Icons from "views/Icons.js";
-import Maps from "views/Maps.js";
 import Notifications from "views/Notifications.js";
 import Upgrade from "views/Upgrade.js";
 
@@ -59,20 +55,6 @@ const dashboardRoutes = [
     name: "Typography",
     icon: "nc-icon nc-paper-2",
     component: Typography,
-    layout: "/admin"
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "nc-icon nc-atom",
-    component: Icons,
-    layout: "/admin"
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "nc-icon nc-pin-3",
-    component: Maps,
     layout: "/admin"
   },
   {
