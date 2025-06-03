e23d8fb260157caf3b32e0f26675db2162dadbae
ArtSahiti <104899505+ArtSahiti@users.noreply.github.com>
2025-06-03 14:22:21 +0200

    delay fix

--- FrontBack/AFront/MedicareF/src/components/Notifications/NotificationDropdown.js
+++ FrontBack/AFront/MedicareF/src/components/Notifications/NotificationDropdown.js
@@ -10,8 +10,8 @@ const NotificationDropdown = () => {
 
   useEffect(() => {
     fetchNotifications();
-    // Poll for new notifications every 30 seconds
-    const interval = setInterval(fetchNotifications, 30000);
+    // Poll for new notifications every 5 seconds
+    const interval = setInterval(fetchNotifications, 5000);
     return () => clearInterval(interval);
   }, []);
 
